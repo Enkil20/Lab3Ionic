4236a27beac19c00cd678602ff26bb84d683b0a6
Arturo Salinas Ortiz <salor_arturo@hotmail.com>
2018-03-15 20:25:54 -0600

    Created the necessary pages for the lab
    Buttons need functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { Page1Page } from '../pages/page1/page1';
+import { Page2Page } from '../pages/page2/page2';
+import { Page3Page } from '../pages/page3/page3';
+import { Page4Page } from '../pages/page4/page4';
+import { Page5Page } from '../pages/page5/page5';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = Page5Page;
+  
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
